a0d427dcd2d827fb1acf6801975b6164f0ee22f9
dev24mxpertz <dev24.mxpertz@gmail.com>
2024-04-20 12:12:36 +0530

    first commit

---
+++ models/CopyData.js
@@ -0,0 +1,57 @@
+const mongoose = require("mongoose");
+
+const storySchema = new mongoose.Schema({
+  Storyimage: [],
+  Paragraph: [],
+});
+
+const wordExploreSchema = new mongoose.Schema({
+  Storytitle: String,
+  Storyttext: String,
+  Storyimage: [],
+  Storyitext: String,
+  Synonyms: {
+    type: String,
+  },
+  Antonyms: {
+    type: String,
+  },
+  isMatched: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const brainQuestSchema = new mongoose.Schema({
+  Question: String,
+  Option: [],
+  Answer: String,
+  isCompleted: { type: Boolean, default: false },
+});
+
+const DataSchema = new mongoose.Schema({
+  Title: { type: String },
+  Image: [],
+  Status: String,
+  Storyadvenure: {
+    Storytitle: String,
+    content: [storySchema],
+  },
+  Wordexplore: [wordExploreSchema],
+  Brainquest: [brainQuestSchema],
+});
+
+const CopyDataSchema = new mongoose.Schema({
+  Student_ID: {
+    type: mongoose.Types.ObjectId,
+    ref: "student",
+  },
+  pathname: {
+    type: String,
+  },
+  Data: [DataSchema],
+});
+
+const CopyData = mongoose.model("CopyData", CopyDataSchema);
+
+module.exports = CopyData;
